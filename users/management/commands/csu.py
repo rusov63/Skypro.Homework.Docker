e19d2b3eb5b308880c3e58d774fc493c9713dfff
rusov63 <rusov63@yahoo.com>
2023-07-24 20:48:29 +0400

    add 24.1 Вьюсеты и дженерики

---
+++ users/management/commands/csu.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from users.models import User
+import os
+
+
+PASSWORD = os.getenv('PASS_SQL') # введите пароль
+EMAIL_USER = os.getenv('EMAIL_USER'), # укажите почту для входа в админ панель
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email= EMAIL_USER,
+            first_name='Admin',
+            last_name='SuperUser',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password(PASSWORD)
+        user.save()
